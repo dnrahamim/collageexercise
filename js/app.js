c737685096611e4c870c12fab2ce4bf76d631022
Dorian Rahamim <drahamim@greencheckverified.com>
2018-08-13 20:43:11 -0400

    first commit. adds select button and changes erase behavior

---
+++ js/app.js
@@ -0,0 +1,104 @@
+var app = {
+  initDone: false,
+  selectedIndex: -1,
+  mode: 'line',
+  lines: [],
+  pos: null,
+  
+  init: function() {
+    var self = this;
+    if(self.initDone)
+      return;
+    self.bindToolbarEvents();
+    self.bindDrawAreaEvents();
+    self.initDone = true;
+  },
+  
+  bindToolbarEvents: function() {
+    var self = this;
+    document.getElementById('btn-line').addEventListener('click', function() {
+      self.mode = 'line';
+      self.pos = null;
+      self.updateToolbarState();
+    });
+    document.getElementById('btn-select').addEventListener('click', function() {
+      self.mode = 'select';
+      self.pos = null;
+      self.updateToolbarState();
+    });
+    document.getElementById('btn-erase').addEventListener('click', function() {
+      if(self.selectedIndex >= 0) {
+        self.lines.splice(self.selectedIndex, 1);
+      }
+      self.deselectLine();
+      self.pos = null;
+      self.render();
+    });
+  },
+  
+  updateToolbarState: function() {
+    var self = this;
+    document.getElementById('btn-line').className = self.mode === 'line' ? 'active' : '';
+    document.getElementById('btn-select').className = self.mode === 'select' ? 'active' : '';
+  },
+
+  selectLine(index) {
+    this.selectedIndex = index;
+  },
+
+  deselectLine() {
+    this.selectedIndex = -1;
+  },
+  
+  bindDrawAreaEvents: function() {
+    var self = this;
+    var canvas = document.getElementById('canvas');
+    canvas.addEventListener('click', function(e) {
+      var x = e.offsetX, y = e.offsetY;
+      if(self.mode === 'line') {
+        if(!self.pos) {
+          // save first click of the line
+          self.pos = [ x, y ];
+        } else {
+          // create the line and add to the list
+          var x0 = self.pos[0], y0 = self.pos[1];
+          var length = Math.sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0));
+          var line = new Line(x0, y0, x, y, length);
+          self.lines.push(line);
+          self.pos = null;
+        }
+      } else if(self.mode === 'select') {
+        if (self.lines.length > 0) {
+          var closestDistance = 10;
+          var closestIndex = -1;
+          self.lines.forEach(function(line, index) {
+            var squareDistance = line.squareDistanceFrom(x, y);
+            if(squareDistance <= closestDistance) {
+              closestDistance = squareDistance;
+              closestIndex = index;
+            }
+          });
+          if(closestIndex >= 0) {
+            self.selectLine(closestIndex);
+          } else {
+            self.deselectLine();
+          }
+        }
+      }
+      self.render();
+    });
+  },
+  
+  render: function() {
+    var self = this;
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    self.lines.forEach(function(line) {
+      line.draw(ctx);
+    });
+    if(self.selectedIndex >= 0) {
+      self.lines[self.selectedIndex].drawEnds(ctx);
+    }
+  },
+};
